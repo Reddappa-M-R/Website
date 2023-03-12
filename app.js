18a2b477bf63cb592eebce1b2c171281bc373ff1
rockreddy <reddymr2018@gmail.com>
2023-03-12 23:34:18 +0530

    i

--- app.js
+++
@@ -1,6 +0,0 @@
-const button = document.querySelector('#button');
-const text = document.querySelector('#text');
-
-button.addEventListener('click', function() {
-  text.textContent = "The button has been clicked!";
-});
